0ca3d8d3a25b344bede37d6329a2ace0bbeb876e
Maximilian Gerhards <maximilian.gerhards@medgurus.de>
2023-11-07 12:44:36 +0100

    Add validations for username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
-    validates :username, presence: true, length: { minimum: 6, maximum: 100 }
-    validates :email, presence: true, length: { minimum: 6, maximum: 1000 }
+    validates :username, presence: true, 
+    uniqueness: { case_sensitive: false }, 
+    length: { minimum: 2, maximum: 40 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, 
+    uniqueness: { case_sensitive: false }, 
+    length: { maximum: 100 },
+    format: { with: VALID_EMAIL_REGEX }
 end
