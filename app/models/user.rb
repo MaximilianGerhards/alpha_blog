ad586689d56496690276438e549ed45c9a2104dd
Maximilian Gerhards <maximilian.gerhards@medgurus.de>
2023-11-10 11:15:36 +0100

    add one-to-many-association user-articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+    has_many :articles
     validates :username, presence: true, 
-    uniqueness: { case_sensitive: false }, 
-    length: { minimum: 2, maximum: 40 }
+        uniqueness: { case_sensitive: false }, 
+        length: { minimum: 2, maximum: 40 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, 
-    uniqueness: { case_sensitive: false }, 
-    length: { maximum: 100 },
-    format: { with: VALID_EMAIL_REGEX }
+        uniqueness: { case_sensitive: false }, 
+        length: { maximum: 100 },
+        format: { with: VALID_EMAIL_REGEX }
 end
