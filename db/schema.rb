928ddc5b8bcca6b0d11b1daf9a5dfcea63944a2c
Maximilian Gerhards <maximilian.gerhards@medgurus.de>
2023-12-04 17:43:29 +0100

    Add article-category-association and display categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_26_201558) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_30_161237) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
